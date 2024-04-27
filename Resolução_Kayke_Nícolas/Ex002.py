f2f6dc08fa7f7c98e31af1bea6ca0f0a53c39061
Kayke Nícolas <kaykenicolas007@gmail.com>
2024-04-27 12:13:46 -0300

    Primeiros 3 exercícios
    A resolução completa dos 3 primeiros exercícios propostos pelo Curso em Vídeo, Python.

---
+++ Resolução_Kayke_Nícolas/Ex002.py
@@ -0,0 +1,5 @@
+'''Ex002 - Faça um programa que leia o nome de uma pessoa e mostre uma
+mensagem de boas-vindas.'''
+
+nome = input('Informe o seu nome: ')
+print(f'Seja muito bem vindo, {nome}! É um prazer te conhecer!')
---
+++ Resolução_Kayke_Nícolas/Ex003.py
@@ -0,0 +1,6 @@
+'''Ex003 - Crie um programa que leia dois números e mostre a soma entre eles.'''
+
+print('Escolha dois números a serem somados!')
+n1 = int(input('Primeiro número: '))
+n2 = int(input('Segundo número: '))
+print(f'A soma entre {n1} e {n2} resulta em: {n1 + n2}')
